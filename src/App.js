313c1de35ba9a9cc0c198d021bb5c565656a9ed8
JorgeRudders05 <Jaclscode@email.com>
2023-09-11 19:54:19 -0300

    codigo system

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AbBotao from './components/AbBotao';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AbBotao/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
